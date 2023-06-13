44283d40205672b9b92cd701f43df0a85d30161e
Adrian <adrianshisia@gmail.com>
2023-06-13 13:49:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply
+
+let random = Math.ceil(Math.random() * 10) + 1
+random
+
+let num3 = 22
+let num4 = 9
+let mod = num3 % num4 
+mod
+
+let max = Math.max(16,17,18,19,20)
+max
